376f12e0fc5afe555c0f5dec3a7348dc31fed29b
Kostya Oreshin <koreshin67@gmail.com>
2025-09-07 16:02:30 +0300

    style: format according to rye

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -3,14 +3,17 @@ from filecmp import cmp
 from os import remove
 import pytest
 
+
 def test_get_graph_info_existing_graph():
     actual = get_graph_info("bzip")
     expected = GraphInfo(556, 632, labels=("d", "a"))
     assert actual == expected
 
+
 def test_get_graph_info_non_existent_graph():
     with pytest.raises(FileNotFoundError):
         get_graph_info("ocaml")
+
 
 def test_save_two_cycles_graph():
     save_two_cycles_graph("actual.dot", 3, 4, common_node=1, labels=("a", "b"))
