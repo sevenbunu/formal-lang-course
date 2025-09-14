18b256625a2495094c8f454152c0f6a676e94a0d
Kostya Oreshin <koreshin67@gmail.com>
2025-09-14 12:47:32 +0300

    test: add tests for automaton transition

---
+++ project/automaton_transition.py
@@ -0,0 +1,43 @@
+from typing import Set
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+    State,
+)
+from pyformlang.regular_expression import Regex
+from networkx import MultiDiGraph
+
+
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    regular_expression = Regex(regex)
+    nfa = regular_expression.to_epsilon_nfa()
+    dfa = nfa.to_deterministic()
+    min_dfa = dfa.minimize()
+    return min_dfa
+
+def graph_to_nfa(
+    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
+) -> NondeterministicFiniteAutomaton:
+    nfa = NondeterministicFiniteAutomaton().from_networkx(graph)
+
+    nodes = set(graph.nodes())
+
+    if not start_states:
+        states_to_add = nodes
+    else:
+        states_to_add = start_states
+    for state in states_to_add:
+        if state not in nodes:
+            raise ValueError(f"start state {state} not in graph")
+        nfa.add_start_state(State(state))
+
+    if not final_states:
+        states_to_add = nodes
+    else:
+        states_to_add = final_states
+    for state in states_to_add:
+        if state not in nodes:
+            raise ValueError(f"final state {state} not in graph")
+        nfa.add_final_state(State(state))
+
+    return nfa
